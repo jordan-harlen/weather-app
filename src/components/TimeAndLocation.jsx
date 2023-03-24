3c188bc637f289f6945157663125b0dd8f6a1ddd
Jordan Harlen <jordan_harlen@hotmail.com>
2023-03-24 23:24:25 +1300

    mostly fixed time and date

--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -1,26 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
+import { DateTime } from 'luxon'
 
 function TimeAndLocation() {
   const weatherData = useSelector((redux) => redux.weather)
+  const [localTime, setLocalTime] = useState('')
 
-  const date = new Date(weatherData?.dt * 1000)
-  const options = {
-    weekday: 'short',
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-  }
-  const dateString = date.toLocaleString('en-US', options)
-
-  console.log(dateString)
+  useEffect(() => {
+    async function getTime() {
+      const timeZone = weatherData?.timezone || 0
+      const date = new Date()
+      const utc = date.getTime() + date.getTimezoneOffset() * 60000
+      const local = new Date(utc + timeZone * 1000)
+      const formattedLocalTime = DateTime.fromJSDate(local).toFormat(
+        "cccc, dd LLL yyyy' | Local time: 'hh:mm a"
+      )
+      setLocalTime(formattedLocalTime)
+    }
+    getTime()
+  }, [weatherData?.dt, weatherData?.timezone])
 
   return (
     <>
       <div className="flex items-center justify-center my-6">
-        <p className="text-white text-xl font-extralight">{dateString}</p>
+        <p className="text-white text-xl font-extralight">{localTime}</p>
       </div>
       <div className="flex items-center justify-center my-3">
         <p className="text-white text-3xl font-medium">
