2c78c5415b16b1bcb774055fee8509cce9935028
Jordan Harlen <jordan_harlen@hotmail.com>
2023-03-24 21:29:56 +1300

    trying to get time to display correctly

--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -3,12 +3,24 @@ import { useSelector } from 'react-redux'
 
 function TimeAndLocation() {
   const weatherData = useSelector((redux) => redux.weather)
+
+  const date = new Date(weatherData?.dt * 1000)
+  const options = {
+    weekday: 'short',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  }
+  const dateString = date.toLocaleString('en-US', options)
+
+  console.log(dateString)
+
   return (
     <>
       <div className="flex items-center justify-center my-6">
-        <p className="text-white text-xl font-extralight">
-          Tuesday, 31 May 2022 | Local Time: 12:46 PM
-        </p>
+        <p className="text-white text-xl font-extralight">{dateString}</p>
       </div>
       <div className="flex items-center justify-center my-3">
         <p className="text-white text-3xl font-medium">
