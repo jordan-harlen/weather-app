67f7b16a78999c11a1b8d794ce424efb2746a656
Jordan Harlen <jordan_harlen@hotmail.com>
2023-03-25 01:55:35 +1300

    trying to make responsive design'

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -3,56 +3,55 @@ import React from 'react'
 function Forecast({ title }) {
   return (
     <>
-      <div className="flex items-center justify-start my-6">
+      <div className="flex flex-col items-center justify-start my-6">
         <p className="text-white font-medium uppercase">{title}</p>
-      </div>
-      <hr className="my-2"></hr>
-
-      <div className="flex flex-row items-center justify-between text-white">
-        <div className="flex flex-col items-center justify-center">
-          <p className="font-light text-sm">2:00 PM</p>
-          <img
-            src="http://openweathermap.org/img/wn/01d@2x.png"
-            alt=""
-            className="w-12 my-1"
-          />
-          <p>19°</p>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <p className="font-light text-sm">3:00 PM</p>
-          <img
-            src="http://openweathermap.org/img/wn/01d@2x.png"
-            alt=""
-            className="w-12 my-1"
-          />
-          <p>19°</p>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <p className="font-light text-sm">4:00 PM</p>
-          <img
-            src="http://openweathermap.org/img/wn/01d@2x.png"
-            alt=""
-            className="w-12 my-1"
-          />
-          <p>19°</p>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <p className="font-light text-sm">5:00 PM</p>
-          <img
-            src="http://openweathermap.org/img/wn/01d@2x.png"
-            alt=""
-            className="w-12 my-1"
-          />
-          <p>19°</p>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <p className="font-light text-sm">6:00 PM</p>
-          <img
-            src="http://openweathermap.org/img/wn/01d@2x.png"
-            alt=""
-            className="w-12 my-1"
-          />
-          <p className="font-medium">19°</p>
+        <hr className="my-2"></hr>
+        <div className="flex flex-col items-center justify-between text-white">
+          <div className="flex flex-col items-center justify-center">
+            <p className="font-light text-sm">2:00 PM</p>
+            <img
+              src="http://openweathermap.org/img/wn/01d@2x.png"
+              alt=""
+              className="w-8 my-1"
+            />
+            <p>19°</p>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <p className="font-light text-sm">3:00 PM</p>
+            <img
+              src="http://openweathermap.org/img/wn/01d@2x.png"
+              alt=""
+              className="w-8 my-1"
+            />
+            <p>19°</p>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <p className="font-light text-sm">4:00 PM</p>
+            <img
+              src="http://openweathermap.org/img/wn/01d@2x.png"
+              alt=""
+              className="w-8 my-1"
+            />
+            <p>19°</p>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <p className="font-light text-sm">5:00 PM</p>
+            <img
+              src="http://openweathermap.org/img/wn/01d@2x.png"
+              alt=""
+              className="w-8 my-1"
+            />
+            <p>19°</p>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <p className="font-light text-sm">6:00 PM</p>
+            <img
+              src="http://openweathermap.org/img/wn/01d@2x.png"
+              alt=""
+              className="w-8 my-1"
+            />
+            <p className="font-medium">19°</p>
+          </div>
         </div>
       </div>
     </>
--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -34,20 +34,12 @@ function Inputs() {
   }
 
   return (
-    <div className="flex flex-row justify-center my-6">
-      <form
-        onSubmit={(e) => {
-          handleSubmit(e)
-        }}
-        className="flex flex-row w-3/4 items-center justify-center space-x-4"
-      >
+    <div className="flex flex-col md:flex-row justify-center my-6">
+      <form className="flex flex-row w-full md:w-3/4 items-center justify-center space-x-4">
         <input
           type="text"
           placeholder="Search a city..."
-          onChange={(e) => {
-            handleSearch(e)
-          }}
-          className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase"
+          className="text-xl font-light p-2 w-full md:w-4/5 shadow-xl focus:outline-none capitalize placeholder:lowercase"
         />
         <button>
           <UilSearch
@@ -60,20 +52,12 @@ function Inputs() {
           className="text-white cursor-pointer hover:scale-125"
         />
       </form>
-      <div className="flex flex-row w-1/4 items-center justify-center">
-        <button
-          name="celcius"
-          onClick={CelsiusButton}
-          className="text-xl text-white font-light"
-        >
+      <div className="flex flex-row w-full md:w-1/4 items-center justify-center">
+        <button name="celcius" className="text-xl text-white font-light">
           °C
         </button>
         <p className="text-xl text-white mx-1">|</p>
-        <button
-          name="fahrenheit"
-          onClick={FahrenheitButton}
-          className="text-xl text-white font-light"
-        >
+        <button name="fahrenheit" className="text-xl text-white font-light">
           °F
         </button>
       </div>
